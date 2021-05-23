58fe1bb6279ac1cd5ff23d2fbf69459f34ca243a
Driquelme2699 <48743247+AndresGomez99@users.noreply.github.com>
2021-05-22 22:52:56 -0600

    Primer commit
    Primcer commit de la funcionalidad de "horas a redaccion"

--- horas.js
+++ horas.js
@@ -1,1 +1,95 @@
 
+
+const numbersNames= ['en punto','uno','dos','tres','cuatro','cinco','seis','siete','ocho','nueve','diez','once','doce',
+'trece','catorce','quince','dieciséis','diecisiete','dieciocho','diecinueve','veinte','veintiuno','veintidós','veintitrés',
+'veinticuatro','veinticinco','veintiséis','veintisiete','veintiocho','veintinueve',]
+
+const quarterNames= ['en punto','cuarto','media','menos cuarto']
+const decimalNumbersNames= ['treinta','cuarenta','cincuenta']
+const dayMomentNames = ['de la mañana','de la tarde','de la noche','media noche','medio día']
+
+
+
+function gethourData(inputHours){
+    let hourData={
+        hour:0,
+        minutes:0,
+        time:""
+    }
+    let splitHours= inputHours.split(':')
+    hourData.hour= splitHours[0];
+    hourData.minutes= splitHours[1].match(/[0-5][0-9]/)[0];
+    hourData.time= splitHours[1].match(/([a-zA-Z][a-zA-Z])/)[0];
+    return hourData;
+}
+
+
+
+
+function getdayMoment(hourData){
+    if(hourData.time=='am' || hourData.time=='AM'){
+        return dayMomentNames[0];
+    }else{
+       if(hourData.hour<6){
+        return dayMomentNames[1];
+       }else{
+        
+        return dayMomentNames[2];
+       }
+    }
+   
+}
+
+function getMinutesString(hourData){
+    let minutesString=''
+    if (hourData.minutes!=0){
+        minutesString+= 'y ';
+    }
+    
+    if(hourData.minutes%15==0){
+        minutesString+=quarterNames[hourData.minutes/15];
+    }else if(hourData.minutes>29){   
+        minutesString+=  decimalNumbersNames[Math.floor(hourData.minutes/10)-3];
+        if(hourData.minutes%10>0){
+            minutesString+= ' y '+ numbersNames[hourData.minutes%10];
+        }
+    }else{
+        console.log("pruebaa",hourData.minutes)
+        minutesString+= numbersNames[Number(hourData.minutes)%30];
+    }
+    return ' '+minutesString+' ';
+}
+
+var hoursModule={
+
+    printHour:function (inputHours){
+        let hourData= gethourData(inputHours);
+        let hourString= '';
+        let minutesString='';
+        if(hourData.hour==1){
+            if(hourData.minutes==0){
+                hourString+='Es la ';
+            }else{
+                hourString+='Son la ';
+            }
+            hourString+='una';
+        }else{
+            if(hourData.hour==12 && hourData.minutes==0){
+                hourString+='Es ';
+                if(hourData.time=='am' || hourData.time=='AM'){
+                    hourString+= dayMomentNames[3];
+                }else{
+                    hourString+= dayMomentNames[4];
+                }
+                return hourString;
+            }
+            hourString+='Son las '+numbersNames[Number(hourData.hour)];
+        }
+        minutesString= getMinutesString(hourData);
+        hourString+= minutesString;
+        hourString+=getdayMoment(hourData);
+        return (hourString);
+    }
+}
+
+module.exports= hoursModule
---
+++ index.js
@@ -0,0 +1,26 @@
+const prompt = require('prompt');
+const { printHour } = require('./horas');
+
+const hourFormat= /(^((0?[1-9])|(1[0-2])):([0-5][0-9])\s?((am|pm)|(AM|PM))$)/
+const properties = [{
+        name: 'hora',
+        validator: hourFormat,
+        warning: 'Hora no está en el formato necesario, por favor vuelva a ingresarla:'
+    },
+  
+];
+console.log('Bienvenido, por favor ingrese una hora con el formato de 12 horas ([horas]:[minutos][am/pm])');
+prompt.start();
+
+prompt.get(properties, function (err, result){
+    if (err){  
+        console.log(err); 
+        return 0;
+    }
+    if (result.hora==""){
+        console.log('No se ingresó ninguna fecha');
+    }else{
+        console.log(printHour(result.hora)+'.');
+    }
+});
+ 
